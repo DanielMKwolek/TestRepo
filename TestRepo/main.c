cd875df898245768cb561453408c4bb364333ce4
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-08-09 20:13:36 -0400

    Initial Commit

---
+++ TestRepo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestRepo
+//
+//  Created by Daniel Kwolek on 8/9/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
